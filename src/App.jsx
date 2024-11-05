dcd81c356939fca694417bab9d56f2f3d9ddaa65
R <secret@example.com>
2024-11-05 09:06:49 -0700

    Lesson 1_1 removed unussed code and added missing h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const todoList =[{id:1,title:"Finish the assignment"},{id:2,title:"Cook"}, {id:3,title:"Do laundry"}];
@@ -11,6 +8,7 @@ function App() {
 
   return (
     <>
+        <h1>Todo List</h1>
       <ul>
           { todoList.map(item => <li key={item.id}>{item.title}</li>)}
       </ul>
